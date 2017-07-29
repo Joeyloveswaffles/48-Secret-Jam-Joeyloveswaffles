bc64fa624ef8b3d6daede10b5601239ec5261149
Joeyloveswaffles <schmidtj18@icloud.com>
2017-07-28 23:32:28 -0500

    Made a masterpiece (created flanders clicking simulator with random placement, timer, and counter)

--- 48 Secret Jam Joeyloveswaffles/Form1.Designer.cs
+++ 48 Secret Jam Joeyloveswaffles/Form1.Designer.cs
@@ -29,11 +29,74 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.flanders_button = new System.Windows.Forms.Button();
+            this.flanders_greeted = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(9, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(150, 17);
+            this.timeLabel.TabIndex = 3;
+            this.timeLabel.Text = "Time Left: 20 Seconds";
+            this.timeLabel.Click += new System.EventHandler(this.timeLabel_Click);
+            // 
+            // flanders_button
+            // 
+            this.flanders_button.BackColor = System.Drawing.Color.White;
+            this.flanders_button.BackgroundImage = global::_48_Secret_Jam_Joeyloveswaffles.Properties.Resources.Ned_Flanders;
+            this.flanders_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flanders_button.ForeColor = System.Drawing.Color.White;
+            this.flanders_button.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.flanders_button.Location = new System.Drawing.Point(12, 43);
+            this.flanders_button.Name = "flanders_button";
+            this.flanders_button.Size = new System.Drawing.Size(150, 200);
+            this.flanders_button.TabIndex = 4;
+            this.flanders_button.UseVisualStyleBackColor = false;
+            this.flanders_button.Click += new System.EventHandler(this.flanders_button_Click);
+            // 
+            // flanders_greeted
+            // 
+            this.flanders_greeted.AutoSize = true;
+            this.flanders_greeted.Location = new System.Drawing.Point(209, 9);
+            this.flanders_greeted.Name = "flanders_greeted";
+            this.flanders_greeted.Size = new System.Drawing.Size(132, 17);
+            this.flanders_greeted.TabIndex = 5;
+            this.flanders_greeted.Text = "Flanders Shooed: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::_48_Secret_Jam_Joeyloveswaffles.Properties.Resources.Simpsons_Door;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.flanders_greeted);
+            this.Controls.Add(this.flanders_button);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Neighbor Greeter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Button flanders_button;
+        private System.Windows.Forms.Label flanders_greeted;
     }
 }
 
--- 48 Secret Jam Joeyloveswaffles/Form1.cs
+++ 48 Secret Jam Joeyloveswaffles/Form1.cs
@@ -12,9 +12,67 @@ namespace _48_Secret_Jam_Joeyloveswaffles
 {
     public partial class Form1 : Form
     {
+        // This integer variable keeps track of the remaining time.
+        int timeLeft = 20;
+        Random random = new Random();
+        int randomXNumber;
+        int randomYNumber;
+        int timeWaited;
+        int greetAmount = 0;
+
+        System.Media.SoundPlayer startSoundPlayer = new System.Media.SoundPlayer(@"C:\Users\Joeyl\OneDrive\Documents\Visual Studio 2015\Projects\48 Secret Jam Joeyloveswaffles\48 Secret Jam Joeyloveswaffles\Resources\HidleyHo.wav");
+
         public Form1()
         {
             InitializeComponent();
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+        }
+        
+        public void randomScreenLocation()
+        {
+            randomXNumber = random.Next(100, 1300);
+            randomYNumber = random.Next(100, 650);
+            timeWaited = random.Next(0, 7);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 1)
+            {
+                // Display the new time left by updating the Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = "Time Left: " + timeLeft + " Seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+            }
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void flanders_button_Click(object sender, EventArgs e)
+        {
+            randomScreenLocation();
+            if (timeLeft > 1)
+            {
+                greetAmount += 1;
+                flanders_greeted.Text = "Flanders Shooed: " + greetAmount;
+                flanders_button.Location = new Point(randomXNumber, randomYNumber);
+            }
+            if (timeLeft == 20)
+            {
+             //Start the timer.
+                timeLeft = 20;
+                timeLabel.Text = "Time Left: 20 Seconds";
+                timer1.Start();
+            }
+            startSoundPlayer.Play();
         }
     }
 }
